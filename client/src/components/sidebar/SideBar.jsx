9de23ceab4db72ca2766645e5e3833c6f3e4e8a3
puskar789 <pbohara231159@gmail.com>
2024-12-24 18:38:58 +0530

    did some code rearrangement

--- client/src/components/sidebar/SideBar.jsx
+++ client/src/components/sidebar/SideBar.jsx
@@ -1,10 +1,43 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { FiAlignJustify } from "react-icons/fi";
 import { useAuthContext } from "../../context/AuthContext";
 import SliderInput from "./SliderInput";
+import useGlobal from "../../zustand/useGlobal";
 
 const SideBar = () => {
   const { containerRef } = useAuthContext();
+  const { rooms, setFilteredRooms, filterLng, filterLat, filterPrice } =
+    useGlobal();
+
+  // filtering the rooms
+  useEffect(() => {
+    const filtered = rooms.filter((room) => {
+      // only price filter
+      if (filterLng === 0 && filterLat === 0 && room.price <= filterPrice) {
+        return true;
+      }
+      if (filterLng === 0 && filterLat === 0 && room.price > filterPrice) {
+        return false;
+      }
+
+      // price and location filter
+      const lngDiff = Math.abs(room.lng - filterLng);
+      const latDiff = Math.abs(room.lat - filterLat);
+
+      if (lngDiff <= 1 && latDiff <= 1) {
+        if (filterPrice < 50) {
+          return room.price <= filterPrice;
+        } else {
+          return true;
+        }
+      } else {
+        return false;
+      }
+    });
+
+    setFilteredRooms(filtered);
+  }, [filterLng, filterLat, filterPrice]);
+
   return (
     <div className="flex-1 drawer z-10">
       <input id="my-drawer" type="checkbox" className="drawer-toggle" />
